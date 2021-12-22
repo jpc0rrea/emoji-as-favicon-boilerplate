4341f34055e1b89b9c917c1fb4608e26efc1769c
Joao Pedro <joaopedrocorrea14@gmail.com>
2021-12-22 13:10:40 -0300

    style: remove some comments

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,6 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* 👇🏽 importação da fonte */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
